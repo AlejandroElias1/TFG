82f4f56b9653865c6bf2589f7fa960616aab01cd
AlejandroElias1 <aleeli889@alu.centroafuera.es>
2023-02-19 19:17:28 +0100

    firebasereg

--- app/src/main/java/com/example/replicaapp/MainActivity2.kt
+++ app/src/main/java/com/example/replicaapp/MainActivity2.kt
@@ -26,5 +26,6 @@ class MainActivity2 : AppCompatActivity() {
         }
 
 
+
     }
 }
--- app/src/main/java/com/example/replicaapp/MainActivity4.kt
+++ app/src/main/java/com/example/replicaapp/MainActivity4.kt
@@ -3,6 +3,7 @@ package com.example.replicaapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.replicaapp.databinding.ActivityMain4Binding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity4 : AppCompatActivity() {
     lateinit var binding: ActivityMain4Binding
@@ -18,8 +19,41 @@ class MainActivity4 : AppCompatActivity() {
             val intent = android.content.Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
+        setup()
     }
 
+    private fun setup() {
+        title = "Autenticacion"
+        binding.signUpButton.setOnClickListener {
+            if (binding.correoInt.text.isNotEmpty() && binding.pass.text?.isNotEmpty() == true) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.correoInt.text.toString(),
+                    binding.pass.text.toString()
+                ).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        binding.signUpButton.setOnClickListener {
+                            val intent = android.content.Intent(this, MainActivity6::class.java)
+                            startActivity(intent)
+                        }
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    private fun showAlert() {
+        val builder = android.app.AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: android.app.AlertDialog = builder.create()
+        dialog.show()
+    }
 
 
 
